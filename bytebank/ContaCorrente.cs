8c7a62283031897a527246d251804d8fb8e5f4da
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-05-19 15:02:58 -0300

    OOP

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -8,9 +8,43 @@ namespace bytebank
 {
     public class ContaCorrente
     {
-        int numero_agencia;
-        string conta;
-        string titular;
-        double saldo;
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+        public ContaCorrente(){} //Empty constructor
+
+        public ContaCorrente(int numero_agencia, string conta, string titular, double saldo)
+        {
+            this.numero_agencia = numero_agencia;
+            this.conta = conta;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public void Depositar(double deposito)
+        {
+            this.saldo += deposito;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Transferir(ContaCorrente ccRecebe, double valor)
+        {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+                ccRecebe.saldo += valor;
+            }
+        }
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,4 +1,34 @@
-﻿int numero_agencia;
-string conta;
-string titular;
-double saldo;
+﻿using bytebank;
+using System.Runtime.CompilerServices;
+
+ContaCorrente contaGabriel = new ContaCorrente();
+contaGabriel.numero_agencia = 123;
+contaGabriel.conta = "1919-Y";
+contaGabriel.titular = "Gabriel Yasuda";
+contaGabriel.saldo = 100;
+
+Console.WriteLine("Conta do {0}", contaGabriel.titular);
+Console.WriteLine("Numero agencia: {0}", contaGabriel.numero_agencia);
+Console.WriteLine("Conta: {0}", contaGabriel.conta);
+Console.WriteLine("Titular da conta: {0}", contaGabriel.titular);
+Console.WriteLine("Saldo: {0:0.0}", contaGabriel.saldo);
+
+Console.WriteLine("\nOPERAÇÕES\n----------");
+
+//contaGabriel.Depositar(100);
+//Console.WriteLine("Novo saldo: {0:0.0}", contaGabriel.saldo);
+
+//if (contaGabriel.Sacar(30) == true)
+//{
+//    Console.WriteLine("Dinheiro sacado com sucesso!");
+//} else
+//{
+//    Console.WriteLine("Saldo insificiente!");
+//}
+
+Console.WriteLine("TRANSFERENCIA");
+ContaCorrente contaLaura = new ContaCorrente(123, "3000-A", "Laura Gracie", 0);
+
+contaGabriel.Transferir(contaLaura, 99.50);
+Console.WriteLine("Saldo Gabriel: {0:0.0}", contaGabriel.saldo);
+Console.WriteLine("Saldo Laura: {0:0.0}", contaLaura.saldo);
